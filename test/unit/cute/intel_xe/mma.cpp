bb9d237074102fb9d113534be81f80a610d73a9e
yhma <yuanhang.ma@intel.com>
2025-12-18 14:09:19 +0800

    Fix thread count mismatch in MMA unit tests

--- test/unit/cute/intel_xe/mma.cpp
+++ test/unit/cute/intel_xe/mma.cpp
@@ -53,12 +53,12 @@ void run_mma_test(ShapeMNK shape_mnk, LayoutShape layout_shape) {
 
 TEST(PVC_CuTe_Xe, MMA_XE_8x16x32_S32S8S8S32_TT) {
   run_mma_test<XE_8x16x32_S32S8S8S32_TT, int8_t, int8_t, int32_t>(
-    Shape<_128, _128, _16>{}, Shape<_2, _2, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_4, _2, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_XE_4x16x32_S32S8S8S32_TT) {
   run_mma_test<XE_4x16x32_S32S8S8S32_TT, int8_t, int8_t, int32_t>(
-    Shape<_128, _128, _16>{}, Shape<_2, _2, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_4, _2, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_XE_2x16x32_S32S8S8S32_TT) {
@@ -68,17 +68,17 @@ TEST(PVC_CuTe_Xe, MMA_XE_2x16x32_S32S8S8S32_TT) {
 
 TEST(PVC_CuTe_Xe, MMA_XE_1x16x32_S32S8S8S32_TT) {
   run_mma_test<XE_1x16x32_S32S8S8S32_TT, int8_t, int8_t, int32_t>(
-    Shape<_128, _128, _16>{}, Shape<_1, _1, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_8, _1, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_XE_8x16x32_S32U8U8S32_TT) {
   run_mma_test<XE_8x16x32_S32U8U8S32_TT, uint8_t, uint8_t, int32_t>(
-    Shape<_128, _128, _16>{}, Shape<_2, _2, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_4, _2, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_XE_4x16x32_S32U8U8S32_TT) {
   run_mma_test<XE_4x16x32_S32U8U8S32_TT, uint8_t, uint8_t, int32_t>(
-    Shape<_128, _128, _16>{}, Shape<_2, _2, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_4, _2, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_XE_2x16x32_S32U8U8S32_TT) {
@@ -88,7 +88,7 @@ TEST(PVC_CuTe_Xe, MMA_XE_2x16x32_S32U8U8S32_TT) {
 
 TEST(PVC_CuTe_Xe, MMA_XE_1x16x32_S32U8U8S32_TT) {
   run_mma_test<XE_1x16x32_S32U8U8S32_TT, uint8_t, uint8_t, int32_t>(
-    Shape<_128, _128, _16>{}, Shape<_1, _1, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_8, _1, _1>{});
 }
 
 // TODO: This case will fail when export IGC_ExtraOCLOptions="-cl-intel-512-GRF-per-thread" 
@@ -102,13 +102,13 @@ TEST(PVC_CuTe_Xe, MMA_XE_1x16x32_S32U8U8S32_TT) {
 TEST(PVC_CuTe_Xe, MMA_XE_8x16x16_F32BF16BF16F32_TT) {
   run_mma_test<XE_8x16x16_F32BF16BF16F32_TT, 
                cutlass::bfloat16_t, cutlass::bfloat16_t, float>(
-    Shape<_128, _128, _16>{}, Shape<_2, _2, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_4, _2, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_XE_4x16x16_F32BF16BF16F32_TT) {
   run_mma_test<XE_4x16x16_F32BF16BF16F32_TT, 
                cutlass::bfloat16_t, cutlass::bfloat16_t, float>(
-    Shape<_128, _128, _16>{}, Shape<_2, _2, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_4, _2, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_XE_2x16x16_F32BF16BF16F32_TT) {
@@ -120,19 +120,19 @@ TEST(PVC_CuTe_Xe, MMA_XE_2x16x16_F32BF16BF16F32_TT) {
 TEST(PVC_CuTe_Xe, MMA_XE_1x16x16_F32BF16BF16F32_TT) {
   run_mma_test<XE_1x16x16_F32BF16BF16F32_TT, 
                cutlass::bfloat16_t, cutlass::bfloat16_t, float>(
-    Shape<_128, _128, _16>{}, Shape<_1, _1, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_8, _1, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_XE_8x16x16_F32F16F16F32_TT) {
   run_mma_test<XE_8x16x16_F32F16F16F32_TT, 
                cutlass::half_t, cutlass::half_t, float>(
-    Shape<_128, _128, _16>{}, Shape<_2, _2, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_4, _2, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_XE_4x16x16_F32F16F16F32_TT) {
   run_mma_test<XE_4x16x16_F32F16F16F32_TT, 
                cutlass::half_t, cutlass::half_t, float>(
-    Shape<_128, _128, _16>{}, Shape<_2, _2, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_4, _2, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_XE_2x16x16_F32F16F16F32_TT) {
@@ -144,19 +144,19 @@ TEST(PVC_CuTe_Xe, MMA_XE_2x16x16_F32F16F16F32_TT) {
 TEST(PVC_CuTe_Xe, MMA_XE_1x16x16_F32F16F16F32_TT) {
   run_mma_test<XE_1x16x16_F32F16F16F32_TT, 
                cutlass::half_t, cutlass::half_t, float>(
-    Shape<_128, _128, _16>{}, Shape<_1, _1, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_8, _1, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_XE_8x16x8_F32TF32TF32F32_TT) {
   run_mma_test<XE_8x16x8_F32TF32TF32F32_TT, 
                cutlass::tfloat32_t, cutlass::tfloat32_t, float>(
-    Shape<_128, _128, _16>{}, Shape<_2, _2, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_4, _2, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_XE_4x16x8_F32TF32TF32F32_TT) {
   run_mma_test<XE_4x16x8_F32TF32TF32F32_TT, 
                cutlass::tfloat32_t, cutlass::tfloat32_t, float>(
-    Shape<_128, _128, _16>{}, Shape<_2, _2, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_4, _2, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_XE_2x16x8_F32TF32TF32F32_TT) {
@@ -168,24 +168,24 @@ TEST(PVC_CuTe_Xe, MMA_XE_2x16x8_F32TF32TF32F32_TT) {
 TEST(PVC_CuTe_Xe, MMA_XE_1x16x8_F32TF32TF32F32_TT) {
   run_mma_test<XE_1x16x8_F32TF32TF32F32_TT, 
                cutlass::tfloat32_t, cutlass::tfloat32_t, float>(
-    Shape<_128, _128, _16>{}, Shape<_1, _1, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_8, _1, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, FMA_XE_UniversalFMA_F32F32F32F32) {
   run_mma_test<UniversalFMA<float, float, float, float>, float, float, float>(
-    Shape<_128, _128, _16>{}, Shape<_1, _1, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_8, _1, _1>{});
 }
 
 #if (IGC_VERSION_MAJOR > 2) || (IGC_VERSION_MAJOR == 2 && IGC_VERSION_MINOR >= 18)
 
 TEST(PVC_CuTe_Xe, MMA_DPAS_S8_8x16) {
   run_mma_test<XE_DPAS_TT<8, int32_t, int8_t>, int8_t, int8_t, int32_t>(
-    Shape<_128, _128, _16>{}, Shape<_2, _2, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_4, _2, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_DPAS_S8_4x16) {
   run_mma_test<XE_DPAS_TT<4, int32_t, int8_t>, int8_t, int8_t, int32_t>(
-    Shape<_128, _128, _16>{}, Shape<_2, _2, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_4, _2, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_DPAS_S8_2x16) {
@@ -195,17 +195,17 @@ TEST(PVC_CuTe_Xe, MMA_DPAS_S8_2x16) {
 
 TEST(PVC_CuTe_Xe, MMA_DPAS_S8_1x16) {
   run_mma_test<XE_DPAS_TT<1, int32_t, int8_t>, int8_t, int8_t, int32_t>(
-    Shape<_128, _128, _16>{}, Shape<_1, _1, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_8, _1, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_DPAS_U8_8x16) {
   run_mma_test<XE_DPAS_TT<8, int32_t, uint8_t>, uint8_t, uint8_t, int32_t>(
-    Shape<_128, _128, _16>{}, Shape<_2, _2, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_4, _2, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_DPAS_U8_4x16) {
   run_mma_test<XE_DPAS_TT<4, int32_t, uint8_t>, uint8_t, uint8_t, int32_t>(
-    Shape<_128, _128, _16>{}, Shape<_2, _2, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_4, _2, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_DPAS_U8_2x16) {
@@ -215,19 +215,19 @@ TEST(PVC_CuTe_Xe, MMA_DPAS_U8_2x16) {
 
 TEST(PVC_CuTe_Xe, MMA_DPAS_U8_1x16) {
   run_mma_test<XE_DPAS_TT<1, int32_t, uint8_t>, uint8_t, uint8_t, int32_t>(
-    Shape<_128, _128, _16>{}, Shape<_1, _1, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_8, _1, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_DPAS_BF16_8x16) {
   run_mma_test<XE_DPAS_TT<8, float, cutlass::bfloat16_t>, 
                cutlass::bfloat16_t, cutlass::bfloat16_t, float>(
-    Shape<_128, _128, _16>{}, Shape<_2, _2, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_4, _2, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_DPAS_BF16_4x16) {
   run_mma_test<XE_DPAS_TT<4, float, cutlass::bfloat16_t>, 
                cutlass::bfloat16_t, cutlass::bfloat16_t, float>(
-    Shape<_128, _128, _16>{}, Shape<_2, _2, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_4, _2, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_DPAS_BF16_2x16) {
@@ -239,19 +239,19 @@ TEST(PVC_CuTe_Xe, MMA_DPAS_BF16_2x16) {
 TEST(PVC_CuTe_Xe, MMA_DPAS_BF16_1x16) {
   run_mma_test<XE_DPAS_TT<1, float, cutlass::bfloat16_t>, 
                cutlass::bfloat16_t, cutlass::bfloat16_t, float>(
-    Shape<_128, _128, _32>{}, Shape<_1, _1, _1>{});
+    Shape<_128, _128, _32>{}, Shape<_8, _1, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_DPAS_F16_8x16) {
   run_mma_test<XE_DPAS_TT<8, float, cutlass::half_t>, 
                cutlass::half_t, cutlass::half_t, float>(
-    Shape<_128, _128, _16>{}, Shape<_2, _2, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_4, _2, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_DPAS_F16_4x16) {
   run_mma_test<XE_DPAS_TT<4, float, cutlass::half_t>, 
                cutlass::half_t, cutlass::half_t, float>(
-    Shape<_128, _128, _16>{}, Shape<_2, _2, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_4, _2, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_DPAS_F16_2x16) {
@@ -263,19 +263,19 @@ TEST(PVC_CuTe_Xe, MMA_DPAS_F16_2x16) {
 TEST(PVC_CuTe_Xe, MMA_DPAS_F16_1x16) {
   run_mma_test<XE_DPAS_TT<1, float, cutlass::half_t>, 
                cutlass::half_t, cutlass::half_t, float>(
-    Shape<_128, _128, _16>{}, Shape<_1, _1, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_8, _1, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_DPAS_TF32_8x16) {
   run_mma_test<XE_DPAS_TT<8, float, cutlass::tfloat32_t>, 
                cutlass::tfloat32_t, cutlass::tfloat32_t, float>(
-    Shape<_128, _128, _16>{}, Shape<_2, _2, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_4, _2, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_DPAS_TF32_4x16) {
   run_mma_test<XE_DPAS_TT<4, float, cutlass::tfloat32_t>, 
                cutlass::tfloat32_t, cutlass::tfloat32_t, float>(
-    Shape<_128, _128, _16>{}, Shape<_2, _2, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_4, _2, _1>{});
 }
 
 TEST(PVC_CuTe_Xe, MMA_DPAS_TF32_2x16) {
@@ -287,7 +287,7 @@ TEST(PVC_CuTe_Xe, MMA_DPAS_TF32_2x16) {
 TEST(PVC_CuTe_Xe, MMA_DPAS_TF32_1x16) {
   run_mma_test<XE_DPAS_TT<1, float, cutlass::tfloat32_t>, 
                cutlass::tfloat32_t, cutlass::tfloat32_t, float>(
-    Shape<_128, _128, _16>{}, Shape<_1, _1, _1>{});
+    Shape<_128, _128, _16>{}, Shape<_8, _1, _1>{});
 }
 
 #if defined(SYCL_INTEL_TARGET) && (SYCL_INTEL_TARGET == 35)
